b6c550e36768f170d1a735837190e1fa90c7c188
Heba Elkasaby <hebaelkasaby@Hebas-MacBook-Air.local>
2021-03-08 14:32:51 -0600

    working in the user route

---
+++ db/models/user.js
@@ -0,0 +1,22 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    fullName: {
+     type: DataTypes.STRING(50),
+     allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      unique: true
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.binary,
+      allowNull: false
+    }
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { asyncHandler, csrfProtection } = require('./utils');
+const db = require('../db/models');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/user/register', csrfProtection, (req, res) => {
+  const user = db.User.build();
+  res.render("user-register", { title: "Sign up for ShowMe", user, csrfToken: req.csrfToken()});
+
+})
 
 module.exports = router;
