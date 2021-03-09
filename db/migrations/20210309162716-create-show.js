7e88fdd35fac16eb3c13ba6b5ae81938f9573e77
Ashley Brown <ashleybrown101789@gmail.com>
2021-03-09 08:40:50 -0800

    created show table

---
+++ db/migrations/20210309162716-create-show.js
@@ -0,0 +1,49 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Shows', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING(70),
+        unique: true
+      },
+      synopsis: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      yearRealeased: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      numberOfSeasons: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      genre: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      imageURL: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Shows');
+  }
+};
