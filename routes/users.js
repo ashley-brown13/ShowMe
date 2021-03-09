09da0271f1febb1aa63dc11b8963822b0e3b67c2
Mauro Sanchez <novamauro98@gmail.com>
2021-03-09 14:34:32 -0600

    Fixed error with belongsToMany in shows table.

--- routes/users.js
+++ routes/users.js
@@ -44,44 +44,45 @@ router.post('/users/signup', csrfProtection, signUpValidators, asyncHandler(asyn
   };
 }))
 
-  router.get('/users/login', csrfProtection, (req, res) => {
-    res.render('user-login', {title: "Sign In Page", csrfToken: req.csrfToken(),})
-  });
+router.get('/users/login', csrfProtection, (req, res) => {
+  res.render('user-login', {title: "Sign In Page", csrfToken: req.csrfToken(),})
+});
 
-  router.post('/users/login', csrfProtection, loginValidators, asyncHandler(async(req, res, next) => {
-    const { email, password } = req.body;
-    let errors = [];
-    const validatorErrors = validationResult(req);
-    if(validatorErrors.isEmpty()){
-      const user = await db.User.findOne({where: {email}});
-      if(user !== null){
-        const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
-        if(passwordMatch){
-          loginUser(req, res, user)
-          return req.session.save((error) => {
-            if(error){
-              next(error)
-            }
-            return res.redirect('/')
-          })
-        }
+router.post('/users/login', csrfProtection, loginValidators, asyncHandler(async(req, res, next) => {
+  const { email, password } = req.body;
+  let errors = [];
+  const validatorErrors = validationResult(req);
+  if(validatorErrors.isEmpty()){
+    const user = await db.User.findOne({where: {email}});
+    if(user !== null){
+      const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
+      if(passwordMatch){
+        loginUser(req, res, user)
+        return req.session.save((error) => {
+          if(error){
+            next(error)
+          }
+          return res.redirect('/')
+        })
       }
-      errors.push('Login failed for the provided email and password.');
-    }else {
-      errors = validatorErrors.array().map(error => error.msg);
     }
-    res.render('user-login', {
-      title: "Sign In Page",
-      email,
-      errors,
-      csrfToken: req.csrfToken()
-    })
-  }))
+    errors.push('Login failed for the provided email and password.');
+  }else {
+    errors = validatorErrors.array().map(error => error.msg);
+  }
+  res.render('user-login', {
+    title: "Sign In Page",
+    email,
+    errors,
+    csrfToken: req.csrfToken()
+  })
+}))
 
-  router.post('/user/logout', (req, res) => {
-    logoutUser(req, res);
-    res.redirect('/');
-  });
+router.post('/user/logout', (req, res) => {
+  logoutUser(req, res);
+  res.redirect('/');
+});
+
 
 
 module.exports = router;
