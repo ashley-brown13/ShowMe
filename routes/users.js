2179aa2ee5ed0e0758db27c88efc42f29c529a6c
Ashley Brown <ashleybrown101789@gmail.com>
2021-03-08 16:20:28 -0800

    wfinished login/logout

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const db = require('../db/models');
 const { signUpValidators, loginValidators } = require('./validators');
 const { validationResult} = require('express-validator');
 const bcrypt = require('bcryptjs')
+const { loginUser, logoutUser } = require('../auth')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -17,7 +18,7 @@ router.get('/users/signup', csrfProtection, (req, res) => {
 
 });
 
-router.post('/users/signup', csrfProtection, signUpValidators, asyncHandler(async(req, res) => {
+router.post('/users/signup', csrfProtection, signUpValidators, asyncHandler(async(req, res, next) => {
   const {fullName, email, password} = req.body;
   const user = db.User.build({fullName, email});
   const validatorErrors = validationResult(req);
@@ -26,7 +27,12 @@ router.post('/users/signup', csrfProtection, signUpValidators, asyncHandler(asyn
     user.hashedPassword = hashedPassword;
     await user.save();
     loginUser(req, res, user);
-    res.redirect('/')
+    return req.session.save((error) => {
+      if(error){
+        next(error)
+      }
+      return res.redirect('/')
+    })
   }else {
     const errors = validatorErrors.array().map(error => error.msg);
     res.render('user-signup', {
@@ -36,14 +42,15 @@ router.post('/users/signup', csrfProtection, signUpValidators, asyncHandler(asyn
       csrfToken: req.csrfToken(),
     })
   };
+}))
 
   router.get('/users/login', csrfProtection, (req, res) => {
-    res.render('user-login', {title: "Sign In Page", csrfToken: req.csrfToken()})
+    res.render('user-login', {title: "Sign In Page", csrfToken: req.csrfToken(),})
   });
 
-  router.post('/user/login', csrfProtection, loginValidators, asyncHandler(async(req, res) => {
-    const { email, password} = req.body;
-    const errors = [];
+  router.post('/users/login', csrfProtection, loginValidators, asyncHandler(async(req, res, next) => {
+    const { email, password } = req.body;
+    let errors = [];
     const validatorErrors = validationResult(req);
     if(validatorErrors.isEmpty()){
       const user = await db.User.findOne({where: {email}});
@@ -51,18 +58,30 @@ router.post('/users/signup', csrfProtection, signUpValidators, asyncHandler(asyn
         const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
         if(passwordMatch){
           loginUser(req, res, user)
-          return res.redirect('/')
+          return req.session.save((error) => {
+            if(error){
+              next(error)
+            }
+            return res.redirect('/')
+          })
         }
       }
       errors.push('Login failed for the provided email and password.');
     }else {
-      errors = validatorErrors.array.map(error => error.msg);
-
+      errors = validatorErrors.array().map(error => error.msg);
     }
+    res.render('user-login', {
+      title: "Sign In Page",
+      email,
+      errors,
+      csrfToken: req.csrfToken()
+    })
   }))
 
+  router.post('/user/logout', (req, res) => {
+    logoutUser(req, res);
+    res.redirect('/');
+  });
 
-
-}))
 
 module.exports = router;
