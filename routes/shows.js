eb33eeedbd524087f4e061a3d4c1efee47e16b98
Sean Rader <seanrader@Seans-MacBook-Pro.local>
2021-03-10 15:37:08 -0800

    Reviews 1.0 are finished

--- routes/shows.js
+++ routes/shows.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const {asyncHandler, csrfProtection} = require('./utils')
+const { validationResult } = require('express-validator');
+const { asyncHandler, csrfProtection } = require('./utils');
 const db = require('../db/models');
+const {  reviewValidators } = require('./validators');
 const router = express.Router();
 
 router.get('/shows/:id(\\d+)', asyncHandler(async(req, res) => {
@@ -13,15 +15,28 @@ router.get('/shows/:id(\\d+)', asyncHandler(async(req, res) => {
   res.render('show', {title: show.title, show, reviews})
 }))
 
-router.get('/reviews/:id(\\d+)', asyncHandler(async (req, res) => {
-  const review = await db.Review.findByPk(req.params.id, {
-    include: db.Show
-  });
-  res.render('reviews', { review })
+router.get('/shows/:id(\\d+)/reviews', csrfProtection, asyncHandler(async (req, res) => {
+  const show = await db.Show.findByPk(req.params.id);
+  const review = await db.Review.build();
+  res.render('create-review', { show, review, csrfToken: req.csrfToken() })
 }))
 
-router.post('/reviews/:id(\\d+)', asyncHandler(async (req, res) => {
-
+router.post('/shows/:id(\\d+)/reviews', csrfProtection, reviewValidators, asyncHandler(async (req, res) => {
+  const { title, comment } = req.body
+  const review = await db.Review.build({
+    title,
+    comment,
+    showId: req.params.id,
+    userId: res.locals.user.id
+  })
+  const validationErrors = validationResult(req)
+  if (validationErrors.isEmpty()) {
+    await review.save();
+    return res.redirect(`/shows/${req.params.id}`)
+  } else {
+    const errors = validationErrors.array().map(error => error.msg);
+    res.render('create-review', { show, review, csrfToken: req.csrfToken(), errors })
+  }
 }))
 
 
