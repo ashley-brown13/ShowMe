74f349a5b22987050940cbfa9667722aaedcd9f5
Savannah Rice <savannah.rice@gmail.com>
2021-03-11 09:59:44 -0700

    Added add to show shelves button on show page

--- public/javascripts/show.js
+++ public/javascripts/show.js
@@ -21,3 +21,29 @@ editButtons.forEach(editButton => {
     window.location.href = `/reviews/${reviewId}`
   })
 })
+
+const addToShowShelfBtn = document.querySelector('.addToShowShelfBtn')
+
+addToShowShelfBtn.addEventListener('click', async(e) => {
+  console.log('test')
+  const showShelfOption = document.querySelectorAll('.showShelfOption').value;
+  const showShelfId = e.target.previousSibling.value;
+  const userId = e.target.value;
+  const showId = e.target.id;
+  const body = { showShelfId, showId };
+
+  try {
+    const res = await fetch(`/api/showshelves/${showShelfId}`, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+  } catch (err){
+    console.log(err);
+  }
+
+
+
+})
